a6520ef6d65a3bd3b5bc2f55b4302ac954ef6812
fathi-asma <fathiasma76@gmail.com>
2025-11-17 23:09:03 +0530

    added user register and login

---
+++ src/test/java/com/store/fresh_fold/FreshFoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.fresh_fold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshFoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
